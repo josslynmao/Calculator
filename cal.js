204aeb9021b0b276153c1f0a5936330a2d46695f
josslynmao <mao.huifeng@hotmail.com>
2015-11-16 19:58:20 +0800

    123

--- cal.js
+++ cal.js
@@ -1,3 +1,10 @@
+var operate =0;
+
 function show(num){
-	var str = String(document.getElementsByName("numScreen")[0].value)
+	var str = String(document.getElementsByName("numScreen")[0].value);//获得屏幕上显示的数字。
+	if (str == "0" || operate !=0) {
+		str = "";//若获得的屏幕上的数字为零(起始状态)，或者对数字进行了操作，则进行清屏。
+	}
+	str = str + String(num);
+	document.getElementsByName("numScreen")[0].value = str;
 }
