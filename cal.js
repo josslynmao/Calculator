9ba3a8773c79558e8f90c8f3759bf6f7e73fff1d
josslynmao <mao.huifeng@hotmail.com>
2015-11-19 18:03:08 +0800

    12345

--- cal.js
+++ cal.js
@@ -1,12 +1,10 @@
 var num = 0 , result = 0 , numshow = "0"; 
-var operate = 0; //判断输入状态的标志 
-var calculate = 0; //判断计算状态的标志 
-var quit = 0; //防止重复按键的标志 
+var operate = 0; //判断输入状态的标志:输入运算符时，下一次输入数据就会清屏。
 
 function show(num){
 	var str = String(document.getElementsByName("numScreen")[0].value);//获得屏幕上显示的数字。
 	if (str == "0" || operate !=0) {
-		str = "";//若获得的屏幕上的数字为零(起始状态)，或者对数字进行了操作，则进行清屏。
+		str = "";//若获得的屏幕上的数字为零(起始状态)；或者对数字进行了操作，则进行清屏。
 	}
 	str = str + String(num);
 	document.getElementsByName("numScreen")[0].value = str;
@@ -23,7 +21,8 @@ function plus(){
 }
 
 function equal(){
+	operate = 0;
 	numshow = document.getElementsByName("numScreen")[0].value;
-	result = num + numshow;
+	result = Number(num) + Number(numshow);
 	document.getElementsByName("numScreen")[0].value = result;
 }
