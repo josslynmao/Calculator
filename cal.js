4903e3dc6f42400465ae8d6633cc7fe1db9d4b24
josslynmao <mao.huifeng@hotmail.com>
2015-11-19 17:44:51 +0800

    123

--- cal.js
+++ cal.js
@@ -1,4 +1,7 @@
-var operate =0;
+var num = 0 , result = 0 , numshow = "0"; 
+var operate = 0; //判断输入状态的标志 
+var calculate = 0; //判断计算状态的标志 
+var quit = 0; //防止重复按键的标志 
 
 function show(num){
 	var str = String(document.getElementsByName("numScreen")[0].value);//获得屏幕上显示的数字。
@@ -7,6 +10,7 @@ function show(num){
 	}
 	str = str + String(num);
 	document.getElementsByName("numScreen")[0].value = str;
+	num = 0;
 }
 
 function ac(){
@@ -14,6 +18,12 @@ function ac(){
 }
 
 function plus(){
-	operate = 1;
-	
+	operate = 1;//对数字进行了加操作，标记为1，当再次输入数字时会清屏。
+	num = document.getElementsByName("numScreen")[0].value;
+}
+
+function equal(){
+	numshow = document.getElementsByName("numScreen")[0].value;
+	result = num + numshow;
+	document.getElementsByName("numScreen")[0].value = result;
 }
