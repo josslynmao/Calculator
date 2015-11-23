bb53bca4c485bc65e0f133d15bd29e7661aaf06c
josslynmao <mao.huifeng@hotmail.com>
2015-11-23 16:24:17 +0800

    123

--- cal.js
+++ cal.js
@@ -1,13 +1,10 @@
-var num = 0 , result = 0 , calculate = 0,numshow = "0"; 
+var  calculate = 0 , numshow = "0"; 
 var  operate = 0; //判断输入状态的标志，来控制何时清屏:输入运算符时，下一次输入数据就会清屏。
 
 //获取及更新屏幕数字
 function show(num){
 	var str = String(document.getElementsByName("numScreen")[0].value);//获得屏幕上显示的数字。
-	if (num == 0 && operate == 0) {
-		str = "0";
-	}
-	else if (str == "0" || operate !=0) {//再想想
+	if ((str == "0" && num != 0) || operate !=0) {//再想想
 		str = "";//若获得的屏幕上的数字为零(起始状态)；或者对数字进行了操作，则进行清屏。
 		operate = 0;//清屏后操作符置0
 	}
@@ -18,7 +15,7 @@ function show(num){
 //AC
 function allClear(){
 	document.getElementsByName("numScreen")[0].value = 0;
-	num = 0 , result = 0 ,calculate = 0, operate = 0, numshow = "0"; 
+	num = 0 , result = 0 , calculate = 0, operate = 0 , numshow = "0"; 
 }
 
 //取反
@@ -41,6 +38,7 @@ function calculateNumber(cal){
 	operate = 1;//对数字进行了加操作，标记为1，当再次输入数字时会清屏。
 	calculate = cal;
 	num = document.getElementsByName("numScreen")[0].value;
+	document.getElementsByName("numScreen")[0].value = result;
 }
 
 //=
