d1f3534ba520cf9a13171103aee4264d3c4bb771
josslynmao <mao.huifeng@hotmail.com>
2015-11-24 19:18:22 +0800

    jquery snow

--- cal.js
+++ cal.js
@@ -88,3 +88,24 @@ function equal(){
 	}
 	document.getElementsByName("numScreen")[0].value = result;
 }
+
+$(function(){
+  var d="<div class='snow'>❅<div>"
+			setInterval(function(){
+				var f=$(document).width();
+				var e=Math.random()*f-100;
+				var o=0.3+Math.random();
+				var fon=10+Math.random()*30;
+				var l = e - 100 + 200 * Math.random();
+				var k=2000 + 5000 * Math.random();
+				$(d).clone().appendTo(".snowbg").css({
+					left:e+"px",
+					opacity:o,
+					"font-size":fon,
+				}).animate({
+				  top:"400px",
+					left:l+"px",
+					opacity:0.1,
+				},k,"linear",function(){$(this).remove()})
+			},200)
+})
